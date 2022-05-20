2fa607c67b8272e1bf649915d770a9cd25b82bd0
Guochao Xie <117010307@link.cuhk.edu.cn>
2022-05-20 18:31:34 +0200

    transition on news

--- src/daily-news.js
+++ src/daily-news.js
@@ -1,11 +1,28 @@
 function update_daily_news(coin, date) {
-    document.getElementById("daily-news-cover").classList.add("hide");
-    document.getElementById("daily-news").classList.remove("hide");
+    hide_daily_news();
+    setTimeout(() => {
+        update_daily_news_data(coin, date);
+        setTimeout(() => {
+            show_daily_news();
+        }, 100);
+    }, 100);
+
+}
+
+function hide_daily_news() {
+    d3.selectAll("#daily-news-cover").classed("hide", true);
+    d3.selectAll("#daily-news")
+        .transition()
+        .duration(100)
+        .ease(d3.easeLinear)
+        .style("opacity", 0);
+}
+
+function update_daily_news_data(coin, date) {
     document.getElementById("daily-news-title-text-coin").innerHTML = coin;
     document.getElementById("daily-news-title-text-date").innerHTML = new Date(date).toLocaleDateString("en-US");
 
     document.getElementById("daily-news-icon-img").src = `./coinIcons/${coin.toLowerCase()}.svg`;
-    
     let newDate = new Date(date);
     
     document.getElementById("prev-date").onclick = () => {
@@ -15,4 +32,14 @@ function update_daily_news(coin, date) {
     document.getElementById("next-date").onclick = () => {
         update_daily_news(coin, newDate.setDate(newDate.getDate() + 1))
     };
+}
+
+function show_daily_news() {
+    d3.selectAll("#daily-news")
+        .style("width", "25vw");
+    d3.selectAll("#daily-news")
+        .transition()
+        .duration(200)
+        .ease(d3.easeLinear)
+        .style("opacity", 1);
 }
