d6c942a0fb56e6e1706d0afde97492bb73775d8b
yimjisu <yimjisu99@gmail.com>
2022-05-22 21:39:00 +0200

    get selected options on app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ function whenDocumentLoaded(action) {
 }
 
 whenDocumentLoaded(() => {
+  // Create instance for classes
   var st = new SelectionTab();
+
+  let selectBtn = document.getElementById("selectionButton");
+
+  function refreshPage() {
+    if (st.selectedCoins.length == 0) {
+      alert("Select at least one coin");
+    } else {
+      st.selectBtn();
+
+      let coins = st.selectedCoins;
+      let month = st.selectedMonth;
+      let year = st.selectedYear;
+
+      //Implement the function
+    }
+  }
+
+  selectBtn.addEventListener("click", refreshPage);
 });
