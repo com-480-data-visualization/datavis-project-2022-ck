ed392385f0d2097d76da3faf4d3f478810dfe909
Shiny-Su <suxiaotian31@gmail.com>
2022-05-25 19:12:57 +0200

    Add volume arcs

--- src/coinComparison/coinComparison.js
+++ src/coinComparison/coinComparison.js
@@ -97,15 +97,15 @@ function updateTable(coins) {
   const selected_date = document.getElementsByClassName('date select');
   const year = selected_date[0].lastChild.innerHTML;
   const month = selected_date[1].lastChild.innerHTML;
+  // d3.select("#circleSvg").remove();
   let counter = collect(coins.length, (v) => updateCircles(v))
-  d3.select("#circleSvg").remove();
   coins.forEach(async e => {
     let newRow = coinTable.insertRow(-1);
     let newType = newRow.insertCell(0);
     let newPrice = newRow.insertCell(1);
     let newVolume = newRow.insertCell(2);
     let newPosition = newRow.insertCell(3);
-    let newKeyWords = newRow.insertCell(4);
+    // let newKeyWords = newRow.insertCell(4);
     let img = new Image();
     img.src = 'coinIcons/' + e.toLowerCase() +'.svg';
     
@@ -145,46 +145,103 @@ function updateTable(coins) {
 
 // ------------------------------circles------------------------------
 function updateCircles(v) {
-  // let counter = collect(coins.length, (v) => {
-  var h = 5
-  var w = 5
-
+  d3.select("#circleSvg")
+  .selectAll("svg")
+  .selectAll(".circles")
+  .selectAll("#volume_texts")
+  .remove();
+  const width = 500;
+  const start = 210;
+  const interval = 300;
   var circleSvg = d3.select('#volumeCircle');
-  circleSvg.append('svg')
-    .attr("id", "circleSvg")
-    .attr('width', w)
-    .attr('height', h)
-    .attr('class','radSol');
+  
+  circleSvg.append('text')
+  .text('Monthly Volume')
+  .attr('x', 0)
+  .attr('y', 80)
+  .attr("stroke", "#51c5cf")
+  .attr("stroke-width", 1)
 
-  const data = [ { "x": 50, "y": 100}, { "x": 1000, "y": 100}];
+  circleSvg.selectAll("svg").remove();
+  circleSvg.append('svg')
+  .attr("id", "circleSvg")
+  .attr('width', width)
+  .attr('height', 5)
+  .attr('class','radSol');
+  
+  const data = [ { "x": start, "y": 75}, { "x": start+interval*(v.length-1), "y": 75}];
   const lineGenerator = d3.line()
   .x(d => d.x)
   .y(d => d.y);
-
+  
   circleSvg.append("path")
   .attr("d", lineGenerator(data))
-  .attr("stroke", "blue")
-  .attr("stroke-width", 2)
+  .attr("stroke", "grey")
+  .attr("stroke-width", 1)
   .attr("fill", "none");
-
+  
   circleSvg.selectAll('.circles')
   .data(v)
   .enter()
   .append('circle')
   .attr('r', function(d, i){ return d[1]*1e-8 })
   .attr('class','circles')
-  .attr('cx', function(d, i) { return 80+200*i; })
-  .attr('cy', 100)
+  .attr('cx', function(d, i) { return start+interval*i; })
+  .attr('cy', 75)
   .attr('fill', function(d, i) { return coin_color[d[0].toLowerCase()]; })
   
-  circleSvg.selectAll('text')
+  circleSvg.selectAll('#volume_texts')
   .data(v).enter().append('text')
   .text(function(d, i) {
-    return '$'+parseFloat(d[1]*1e-8).toFixed(2)+'B';
+    return '$'+parseFloat(d[1]*1e-6).toFixed(2)+'M';
   })
   .attr('font-size', 10)
   .attr('fill', 'black')
-  .attr('y', 100)
-  .attr('x', function(d, i){ return 80+200*i; })
+  .attr('y', 75)
+  .attr('x', function(d, i){ return start+interval*i; })
   .attr('text-anchor', 'middle')
+  
+  updateArc(v);
+  
+}
+
+function updateArc(v){
+  volume_arr = []
+  for (const [name, volume] of v) {
+    volume_arr.push(volume);
+  }
+  vs = sum(volume_arr)
+  volume_percent = volume_arr.map(x => (x / vs * 100).toFixed(2))
+  
+  var pie = d3.pie();
+  var width = 500;
+  var outerRadius = width / 2.5;
+  var innerRadius = width / 4;
+  var arc = d3.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+  var arcSvg = d3
+      .select("#volumePercent")
+      .attr("width", width)
+      .attr("height", width)
+      .append("g")
+  var arcs = arcSvg.selectAll("g.arc")
+    .data(pie(volume_percent))
+    .enter()
+    .append("g")
+    .attr("class", "arc")
+    .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")");
+  arcs.append("path")
+    .attr("fill", function(d, i) { return coin_color[v[i][0].toLowerCase()];})
+    .attr("d", arc);
+  arcs.append("text")
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("text-anchor", "middle")
+    .attr('fill', '#51c5cf')
+    .text(function(d) { return d.value + '%'; });
+  arcs.append('text')
+    .text('Sum of Volume')
+    .attr('x', -50)
+    .attr('y', 10)
+    .attr('fill', '#51c5cf')
 }
